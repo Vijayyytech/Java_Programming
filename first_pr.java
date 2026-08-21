98130e4fc3f00c7015c88eb46c6485ae370a4ec7
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-21 18:08:45 +0530

    Added java code

---
+++ first_pr.java
@@ -0,0 +1,8 @@
+public class first_pr{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("The sum of a and b is"+ c);
+    }
+}
