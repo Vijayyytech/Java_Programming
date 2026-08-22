717102c438c5c3608fa0f211233ae95b22965736
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-22 23:58:31 +0530

    practice set-1

---
+++ cgpa.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class cgpa{
+    public static void main(String[] args){
+       // float maths = 90;
+        //float physics = 92;
+        //float chemistry = 90;
+        //float cgpa = (maths + physics + chemistry)/30;
+        //System.out.println(cgpa);
+
+        // problem-02
+        System.out.println("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+        sc.close();
+    }
+}
