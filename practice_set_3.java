80d555b2c7585e38652556a88962c55ebf3aad04
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-23 00:51:18 +0530

    string methods

---
+++ practice_set_3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class practice_set_3 {
+    public static void main(String[] args) {
+        //String name = "Vijju Tripathi";
+        //name = name.toLowerCase();
+        //System.out.println(name);
+
+        //String words = "Hello   my name    is vijju";
+        //words = words.replace( ' ',  '_');
+        //System.out.println(words);
+
+        //String words = "Dear <|name|> Thanks a lot";
+        //words = words.replace( "<|name|>",  "Vijju");
+        //System.out.println(words);
+
+        // to identify double and triple spaces in string
+         //String Mystring = "This is  to  identify   doubles and triples";
+         //System.out.println(Mystring.indexOf("  "));
+         //System.out.println(Mystring.indexOf("   ")); // if it gives -1 then there is no 3 space in string:
+
+         // formatting of string
+         String myletter = "Vijju,\n\tyou are handsome.\nThanks";
+         System.out.println(myletter);
+    }
+}
