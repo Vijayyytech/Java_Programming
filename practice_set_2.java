63e21acc522438bdc521d4da9a69798d319035b4
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-23 00:21:05 +0530

    coding in java

---
+++ practice_set_2.java
@@ -0,0 +1,17 @@
+public class practice_set_2 {
+    public static void main(String[] args){
+        // first_problem->
+       // float a = 7/4.0f * 9/2.0f;
+        // System.out.println(a);
+
+        // second_problem->
+        //Encrypting the grade by 8
+        char grade = 'B';
+        grade = (char)(grade+8);
+        System.out.println(grade);
+
+        //decrypting the grade by 8
+        grade = (char)(grade-8);
+        System.out.println(grade);
+    }
+}
