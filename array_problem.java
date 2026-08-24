205ab37a4123afe2ee72aa7df0d110d7efc21f70
Vijayyytech <vijaytripathi1296@gmail.com>
2026-08-24 16:47:48 +0530

    updates

---
+++ array_problem.java
@@ -0,0 +1,34 @@
+public class array_problem {
+    public static void main(String[] args) {
+        //float [] marks = {78.9f, 56.5f, 90.0f, 89.6f, 67.6f};
+        //float sum = 0;
+        //for (float elements:marks){
+         //   sum = sum + elements;
+            
+        //}
+        //System.out.println(sum);
+
+       /*  int [] numbers = {3,4,5,6,7,8};
+        int check = 10;
+        int flag = 0;
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] == check){
+                System.out.println("number is present");
+                flag++;
+                break;
+            }
+        }
+        if(flag == 0){
+            System.out.println("number is not in array");
+        } */
+
+       /*  int [] marks = {60,85,99,90,95};
+        int sum = 0;
+        for(int element:marks){
+            sum = sum + element;
+        }
+        System.out.println("Average "+sum/marks.length); */
+
+        
+    }
+}
